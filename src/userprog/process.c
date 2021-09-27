92475b830f2d78b6e9c7f56d19a673780cde7f00
Emma Yokota <emmayokota@berkeley.edu>
2021-09-27 02:56:35 +0000

    Checkpoint 1 complete

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -60,13 +60,8 @@ pid_t process_execute(const char* file_name) {
     return TID_ERROR;
   strlcpy(fn_copy, file_name, PGSIZE);
 
-  // Copy file name only up the the first space to ensure process name is correct
-  size_t len = strcspn(file_name, " " ) + 1;
-  char *process_name = malloc(len);
-  strlcpy(process_name, file_name, len);
-
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create(process_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create(file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page(fn_copy);
   return tid;
