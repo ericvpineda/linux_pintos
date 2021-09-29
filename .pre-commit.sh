67431d47da0301608b7eabc63f80a2b47d1cbdd2
Marcus Plutowski <achierius@berkeley.edu>
2021-09-28 17:54:37 -0700

    Patch pre-commit hook

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -6,7 +6,7 @@ if comm -23 <(git diff --cached --name-only --diff-filter=CMRA | sort)\
             <(git diff --name-only --diff-filter=CMRA | sort)\
             | grep '\.[ch]$' > "$files_to_re_add"
 then
-	( cd "$(git rev-parse --show-toplevel)/pintos/src" && make format )
+	( cd "$(git rev-parse --show-toplevel)/src" && make format )
 	xargs git add < "$files_to_re_add"
 fi
 
