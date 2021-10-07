d1943f0e9c50674166ce838ab765c5d28ca597c6
Eric Pineda <ericvpineda@berkeley.edu>
2021-10-07 17:36:39 +0000

    clean up file syscalls

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -31,7 +31,6 @@ struct process {
   int exit_code;              /* Exit code */
   struct file *fdt[128];      /* ADDED: File descriptor table */
   int fd_index;               /* ADDED: Next unused fd index */
-  struct file *executable;
 };
 
 void userprog_init(void);
